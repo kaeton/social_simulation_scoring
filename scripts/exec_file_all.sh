f8589f895c9200c901775abf22cce6367b73485c
Tatsuya Hanyu <s1210099@gmail.com>
2018-02-27 19:44:52 +0900

    [ADD] repeat function

--- scripts/exec_file_all.sh
+++ scripts/exec_file_all.sh
@@ -1,7 +1,7 @@
 input_file_path=$1
 # FILES=($(ls -1 input_file_path))
 FILES=($(ls -1 ${input_file_path}))
-echo ${FILES[*]}
+echo ${#FILES[*]}
 
 input_option () {
     echo ${OPTION}
@@ -15,10 +15,15 @@ input_option () {
         echo "read program file with vim"
         vim ${progfile}
         status=2
+    elif [ ${OPTION} = "e" ]; then
+        echo "read program file with emacs"
+        emacs ${progfile}
+        status=2
     fi
 }
 
-for id in ${FILES[*]}; do 
+for ((i = 0; i < ${#FILES[*]}; i++)) {
+    id=${FILES[i]}
     echo ${id}
     progfile="${input_file_path}/${id}/${id}-1.c"
     if [ ! -f $progfile ]; then
@@ -31,15 +36,17 @@ for id in ${FILES[*]}; do
     fi
 
     while true; do
-        echo "n := next student\nc := Repeat the experiment again\nv := read program file with vim"
+        echo "n := next student\nr := Repeat the experiment again\nv := read program file with vim"
         echo "INPUT OPTION -: "
         read OPTION
         input_option ${OPTION}
         if [ ${status} = 0 ]; then
             break
+        elif [ ${status} = 1 ]; then
+            i=$i-1
+            break
         elif [ ${status} = 2]; then
             continue
         fi
     done
-done
-
+}
